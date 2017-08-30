b1b473699e2fe9ba31018637b9baa8434596cb9a
DianaQuispe <danaalienmas@gmail.com>
2017-08-30 12:11:22 -0500

    d

--- js/main.js
+++ js/main.js
@@ -1,26 +1,87 @@
-var comidas = ['assets/images/f1.png', 'assets/images/f2.png', 'assets/images/f3.png', 'assets/images/f4.png','assets/images/f1.png', 'assets/images/f2.png', 'assets/images/f3.png', 'assets/images/f4.png'];
-comidasEsc = [];
-var image;
-var  u= 0;
-var src;
-var contenedor = document.getElementsByClassName('contenedor')[0];
-for(var i=0; i<comidas.length; i++) {
-    var divs = document.createElement('div');
-    divs.setAttribute('class','imgI');
+   comidas = ['assets/images/f1.png', 'assets/images/f2.png', 'assets/images/f3.png', 'assets/images/f4.png', 'assets/images/f1.png', 'assets/images/f2.png', 'assets/images/f3.png', 'assets/images/f4.png'];
+   var todos;
+   var primero = [];
+   var segundo = [];
+   var tercero = [];
+   var cuarto = [];
+   //  if ($('.primero')) {}
+   //var chatarra = $('.ch');
+   //   var imagenes = $('img');
 
-    var image = document.createElement('img');
-    image.setAttribute('src', 'assets/images/chatarra.jpg');
+   $('img').click(function () {
+       var ids = $(this).parent().attr('id');
+       var ids = parseInt(ids);
+       this.src = comidas[ids - 1];
+       console.log(this.src);
 
-    image.setAttribute('class', 'food');
+   });
+
+   /*
+      var todos = [];
+       todos.push(this.src);
+       console.log(todos);
+     $('img').each(function () {
+
+   })
+      if ($(this).hasClass('primero')) {
+           primero.push(this);
+
+       } else if ($(this).hasClass('segundo')) {
+           segundo.push(this);
+           $(this).remove('imgI');
+
+       } else if ($(this).hasClass('tercero')) {
+           tercero.push(this);
+       } else {
+           cuarto.push(this);
+       };
+       for (var i = 0; i < comidas.length; i++) {
+
+       }
+   comidasEsc = [];
+   var image;
+   var  u= 0;
+   var src;
+   var contenedor = document.getElementsByClassName('contenedor')[0];
+   for(var i=0; i<comidas.length; i++) {
+   var divs = document.createElement('div');
+   divs.setAttribute('class','imgI');
+
+   var image = document.createElement('img');
+   image.setAttribute('src', 'assets/images/chatarra.jpg');
+
+   image.setAttribute('class', 'food clase');
    // comidasEsc = comidas.sort(function() {return Math.random() - 0.5});
-  //  image.setAttribute('src', comidasEsc[i]);
-    divs.appendChild(image);
-    contenedor.appendChild(divs);
-}
-$('.food').click(function() {
-    $(this).attr('src', comidas[u]);
-    var src = this.src;
-u++;
-});
+   //  image.setAttribute('src', comidasEsc[i]);
+   divs.appendChild(image);
+   contenedor.appendChild(divs);
+   }
+   var clicko = 1;
+   var todos = [];
+   $('img').click(function(i) {
+   if(clicko == 1 ) {
+   alert(clicko);
 
+   }
+   clicko++;
 
+   $(this).attr('src', comidas[u]);
+
+   todos.push(this.src);
+
+   for(var t = 0; t<comidas.length; t++) {
+   if(this.src == comidas[t]) {
+   alert((this).src);
+
+   }
+   clicko =0;
+
+   // $(this).fadeOut();
+
+   }
+   var src = this.src;
+   u++;
+
+   });
+
+   */
